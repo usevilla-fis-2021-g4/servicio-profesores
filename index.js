0ddd540b157b5d5ce06fae53f1ce59c2d50239f8
Juan Carlos Estrada Nieto <juancarlosestradanieto@gmail.com>
2021-11-20 19:09:10 +0000

    Added support to nedb for POST method

--- index.js
+++ index.js
@@ -1,21 +1,36 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var DataStore = require("nedb");
 
 var port = 3000;
 var BASE_API_PATH = "/api/v1";
+var DB_FILE_NAME = __dirname + "/profesores.json";
 
+/*
 var profesores = [    
     {"identificacion": "000000", "nombre": "Director", "editable": false},
     {"identificacion": "111111", "nombre": "Fulano", "editable": true},
     {"identificacion": "222222", "nombre": "Mengano", "editable": true},
     {"identificacion": "333333", "nombre": "Zutano", "editable": true}
 ];
+*/
 
 console.log("Starting API server...");
 
 var app = express();
-
 app.use(bodyParser.json());
+
+var db = new DataStore({
+    filename: DB_FILE_NAME,
+    autoload: true
+});
+
+//inserción del diorector
+var profesorDirector = {"identificacion": "000000", "nombre": "Director", "editable": false};
+db.count({"identificacion": "000000"}, function (err, count) {
+    console.log(count);
+    if(count == 0)db.insert(profesorDirector);
+});
 
 app.get("/", (request, response) => {
     response.send("<html><body><h1>My Server.</h1></body></html>");
@@ -23,7 +38,7 @@ app.get("/", (request, response) => {
 
 app.get(BASE_API_PATH+"/profesores", (request, response) => {
     console.log(Date() + "GET - /profesores");
-    response.send(profesores);
+    response.send([]);
 });
 
 //el body llega vacío con postman pero funciona haciendo el post desde terminal
@@ -31,11 +46,21 @@ app.get(BASE_API_PATH+"/profesores", (request, response) => {
 app.post(BASE_API_PATH+"/profesores", (request, response) => {
     console.log(Date() + "POST - /profesores");
     var profesor = request.body;
-    console.log("profesor");
-    console.log(profesor);
-    
-    profesores.push(profesor);
-    response.sendStatus(201);
+    // console.log("profesor");
+    // console.log(profesor);
+
+    db.insert(profesor, function(err, newDoc) {
+        if(err)
+        {
+            console.log(Date() + " - "+err);
+            response.sendStatus(500);
+        }
+        else
+        {
+            response.sendStatus(201);
+        }
+
+    });
 });
 
 app.listen(port);
